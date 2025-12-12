0cbb164cd738cb69959664c9625214bf0fd97d8a
elawadii <elawadym44@example.com>
2025-12-12 19:58:08 +0200

    chmist grade

--- vite-app/src/settings/constants/gradeConstants.js
+++ vite-app/src/settings/constants/gradeConstants.js
@@ -6,6 +6,7 @@ const THIRD_GRADE_TITLE = 'اللغه الالمانيه'
 const gradeConstants = [
     { name: THIRD_GRADE_TITLE, description: 'محتوى (اللغه الالمانيه ) ', index: 1, img: '/assets/germany.webp' },
     { name: FIRST_GRADE_TITLE, description: 'محتوى الصف الاول الثانوى (علوم متكامله) بالوحدات و الكورسات الموجوده', index: 2, img: '/assets/science.jpg' },
+    { name: 'ماده الكيمياء', description: 'محتوى ماده الكيمياء', index: 3, img: '/assets/chemist.webp' },
     // { name: 'الاحياء', description: 'محتوى الصف الثالث الثانوى (احياء) بالوحدات و الكورسات الموجوده', index: 3, img: '/assets/biology.jpg', share: 1 },
 ]
 
